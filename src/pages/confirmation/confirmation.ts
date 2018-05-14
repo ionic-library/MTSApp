6413f215a920d6fba4fe691df42768de1129db10
mblais93 <blai0264@algonquinlive.com>
2018-05-14 15:12:58 -0400

    added confirmation page

---
+++ src/pages/confirmation/confirmation.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-confirmation',
+  templateUrl: 'confirmation.html'
+})
+export class ConfirmationPage {
+
+  constructor(public navCtrl: NavController) { }
+
+}
