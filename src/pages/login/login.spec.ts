814f2ca323eabcbc6971c2fd960ff1b69b3e7502
Calvin Rodo <calvin.rodo@hrsc-rhdcc.gc.ca>
2018-05-17 14:06:04 -0400

    Working on unit tests

--- src/pages/login/login.spec.ts
+++ src/pages/login/login.spec.ts
@@ -3,18 +3,23 @@ import { CommonTestModule } from "../../app/sharedModules";
 import { LoginPage } from "./login";
 import { Ineeda, ineeda } from "ineeda";
 import { User } from "../../providers";
-import { By } from "@angular/platform-browser";
+import * as sinon from 'sinon';
+
+// Test setup:
 
 describe("EI Reporting Service Login Page", () => {
   let sut: ComponentFixture<LoginPage>;
   let comp: LoginPage;
+  let userProvider: User;
+
 
   beforeEach(async () => {
+    userProvider = ineeda.instanceof<User>(User);
     TestBed.configureTestingModule({
       declarations: CommonTestModule.getDeclarations([LoginPage]),
       imports: CommonTestModule.getImports(),
       providers: CommonTestModule.getProviders([
-        { provide: User, useFactory: ineeda.factory<User>() }
+        { provide: User, useClass: userProvider }
       ])
     });
   });
@@ -33,29 +38,14 @@ describe("EI Reporting Service Login Page", () => {
     expect(comp instanceof LoginPage).toBe(true);
   });
 
-  it("Should have 13 provinces and territories", () => {
-    let provinces = sut.debugElement.query(By.css("#residence"));
-    expect(provinces.nativeElement.childNodes.length).toBe(13);
+  it("Should call login on the user provider and pass the SIN, AC, and province of residence when clicked", () => {
+    comp.doLogin();
+
+    sinon.spy(userProvider, 'login');
+
+    expect(userProvider.login).toHaveBeenCalledTimes(1);
   });
 
-  it("Should have all provinces and territories", () => {
-    let provinces = sut.debugElement.query(By.css("#residence"));
-    expect(provinces.childNodes.values).toBe([
-    "AB",
-    "BC",
-    "MB",
-    "NB",
-    "NL",
-    "NS",
-    "ON",
-    "PE",
-    "QC",
-    "SK",
-    "NT",
-    "NU",
-    "YT"
-    ]);
-  });
 
   //TODO: Add test for login success
   //TODO: Add test for login failure
