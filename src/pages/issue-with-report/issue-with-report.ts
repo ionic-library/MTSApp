bc002aa4eeca6166f831cda33ff92deb14061681
mblais93 <blai0264@algonquinlive.com>
2018-05-22 14:30:24 -0400

    removed 'x' typo

--- src/pages/issue-with-report/issue-with-report.ts
+++ src/pages/issue-with-report/issue-with-report.ts
@@ -16,7 +16,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class IssueWithReportPage {
 
   constructor() { }
-x
+
   ionViewDidLoad = () => console.log('ionViewDidLoad IssueWithReportPage');
 
 }
