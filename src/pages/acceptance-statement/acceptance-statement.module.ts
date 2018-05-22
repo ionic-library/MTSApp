d5e175ad5ccba61686d3036e4744f3e943555523
markonnen <jordan.walcaraz@gmail.com>
2018-05-22 11:26:48 -0400

    Added html and css for page elements

--- src/pages/acceptance-statement/acceptance-statement.module.ts
+++ src/pages/acceptance-statement/acceptance-statement.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+import { TranslateModule } from '@ngx-translate/core'
 import { AcceptanceStatementPage } from './acceptance-statement';
 
 @NgModule({
