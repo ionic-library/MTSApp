f33eb417eb174acaf15949012fea7fe3fcfc7c9b
Calvin Rodo <calvin.rodo@hrsc-rhdcc.gc.ca>
2018-05-28 11:42:12 -0400

    Cleaned up tests
    Moved the login button tests back into the home.spec.ts to keep it consistent with other tests.
    Used sub desribes for the login button and basic navigation buttons so we can reuse the setup code.

--- src/pages/home/home.spec.ts
+++ src/pages/home/home.spec.ts
@@ -5,6 +5,8 @@ import { NavController } from "ionic-angular";
 import { NavMock } from "./../../../test-config/mocks-ionic";
 import { SitePages } from "..";
 import { async, TestBed, ComponentFixture } from "@angular/core/testing";
+import { ineeda } from 'ineeda';
+import { User } from "../../providers";
 import * as chai from "chai";
 import * as sinon from "sinon";
 import * as sinonChai from "sinon-chai";
@@ -18,7 +20,7 @@ describe("The Home Page", () => {
   let navSpy: sinon.SinonSpy;
   let fakeNavController: NavMock;
 
-  beforeEach(async () => {
+  function setupTestBedConfig(){
     fakeNavController = new NavMock();
     TestBed.configureTestingModule({
       declarations: CommonTestModule.getDeclarations([HomePage]),
@@ -27,36 +29,75 @@ describe("The Home Page", () => {
         { provide: NavController, useValue: fakeNavController }
       ])
     }).compileComponents();
-  });
+  }
 
-  beforeEach(() => {
+  function createTestObjects(){
     sut = TestBed.createComponent(HomePage);
     homePage = sut.componentInstance;
     navSpy = sinon.spy(fakeNavController, "push");
+  }
+
+  describe("Login Button",() => {
+    let fakeLoginCheck: boolean;
+
+    beforeEach(async () => {
+      ineeda.intercept<User>({
+        isLoggedIn: () => fakeLoginCheck
+      })
+      setupTestBedConfig();
+    });
+
+    beforeEach(() => {
+      createTestObjects();
+    });
+
+
+    it("Should navigate to the EI Reporting Login Page when user is not logged in", () => {
+      fakeLoginCheck = false;
+      homePage.navigateToEIReportingPage();
+      expect(navSpy).to.have.been.calledWith(SitePages.EILogin);
+    });
+
+    it("Should navigate to the EI Reporting Page when user is logged in", () => {
+      fakeLoginCheck = true;
+      homePage.navigateToEIReportingPage();
+      expect(navSpy).to.have.been.calledWith(SitePages.EiReporting);
+    });
   });
 
-  it("Should navigate to the Search Page when navigateToSearchPage is called", () => {
-    homePage.navigateToSearchPage();
-    expect(navSpy).to.have.been.calledWith(SitePages.JobSearch);
-  });
+  describe("Navigation Buttons", () => {
+    beforeEach(async () => {
+      setupTestBedConfig();
+    });
 
-  it("Should navigate to the Service Canada Location navigateToSCCLocationPage is called", () => {
-    homePage.navigateToSCCLocationPage();
-    expect(navSpy).to.have.been.calledWith(SitePages.SCCLocations);
-  });
+    beforeEach(() => {
+      createTestObjects();
+    });
 
-  it("Should navigate to the My Notifications Page when navigateToMyNotificationsPage is called", () => {
-    homePage.navigateToMyNotificationsPage();
-    expect(navSpy).to.have.been.calledWith(SitePages.MyNotifications);
-  });
+    it("Should navigate to the Search Page when navigateToSearchPage is called", () => {
+      homePage.navigateToSearchPage();
+      expect(navSpy).to.have.been.calledWith(SitePages.JobSearch);
+    });
 
-  it("Should navigate to the Benefit Finder Page when navigateToBenefitFinderPage is called", () => {
-    homePage.navigateToBenefitFinderPage();
-    expect(navSpy).to.have.been.calledWith(SitePages.BenefitFinder);
-  });
+    it("Should navigate to the Service Canada Location navigateToSCCLocationPage is called", () => {
+      homePage.navigateToSCCLocationPage();
+      expect(navSpy).to.have.been.calledWith(SitePages.SCCLocations);
+    });
 
-  it("Should navigate to the Life Events Page when navigationToLifeEventsPage is called", () => {
-    homePage.navigateToLifeEventsPage();
-    expect(navSpy).to.have.been.calledWith(SitePages.LifeEvents);
-  });
+    it("Should navigate to the My Notifications Page when navigateToMyNotificationsPage is called", () => {
+      homePage.navigateToMyNotificationsPage();
+      expect(navSpy).to.have.been.calledWith(SitePages.MyNotifications);
+    });
+
+    it("Should navigate to the Benefit Finder Page when navigateToBenefitFinderPage is called", () => {
+      homePage.navigateToBenefitFinderPage();
+      expect(navSpy).to.have.been.calledWith(SitePages.BenefitFinder);
+    });
+
+    it("Should navigate to the Life Events Page when navigationToLifeEventsPage is called", () => {
+      homePage.navigateToLifeEventsPage();
+      expect(navSpy).to.have.been.calledWith(SitePages.LifeEvents);
+    });
+  })
+
 });
