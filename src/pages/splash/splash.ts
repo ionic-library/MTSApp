1b4b2c71bb6f82780a3a80aa3b71c7792cdbe646
Sean Kibbee <seankibbee@gmail.com>
2018-06-11 16:18:54 -0400

    New Changes

--- src/pages/splash/splash.ts
+++ src/pages/splash/splash.ts
@@ -1,32 +1,57 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Lang, User, LangCodes } from '../../providers';
-import { SitePages } from '../../pages';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { Lang, User, LangCodes } from "../../providers";
+import { SitePages } from "../../pages";
 
 @IonicPage()
 @Component({
-  selector: 'page-splash',
-  templateUrl: 'splash.html',
+  selector: "page-splash",
+  templateUrl: "splash.html"
 })
 export class SplashPage {
-
   constructor(public navCtrl: NavController, public user: User) {
-
-    console.log(this.user.isLangSet);
-    if (this.user.isLangSet) {
-      console.log("lang is set: " + user.Lang);
-    }
+    this.user.GetLang(
+      val => {
+        if (typeof val == "undefined") {
+          // User lang is not set
+          console.log("Lang undefined");
+        } else {
+          // user lang is set
+          console.log("Lang Defined " + val);
+        }
+      },
+      val => {
+        // NOT GOOD
+        console.log("Error getting language: " + val);
+      }
+    );
   }
 
   public setEnglish() {
-    console.log(this.user.isLangSet);
-    //this.user.setLang(LangCodes.EN);
-   // this.navCtrl.push(SitePages.Home);
+    this.user.setLang(
+      LangCodes.EN,
+      () => {
+        this.navCtrl.push(SitePages.Home);
+      },
+      val => {
+        console.log("Could not set as English: " + val);
+      }
+    );
+  }
+
+  public setFrench() {
+    this.user.setLang(
+      LangCodes.FR,
+      () => {
+        this.navCtrl.push(SitePages.Home);
+      },
+      val => {
+        console.log("Could not set as French: " + val);
+      }
+    );
   }
 
   ionViewDidLoad() {
-    console.log(this.user.isLangSet);
     //console.log('ionViewDidLoad SplashPage');
   }
-
 }
