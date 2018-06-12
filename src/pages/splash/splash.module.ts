0ead5cb60fab266d176a6a03917b7b734fef9aa4
mblais93 <blai0264@algonquinlive.com>
2018-06-12 13:40:00 -0400

    imported translate module

--- src/pages/splash/splash.module.ts
+++ src/pages/splash/splash.module.ts
@@ -1,13 +1,10 @@
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { SplashPage } from './splash';
+import { NgModule } from "@angular/core";
+import { IonicPageModule } from "ionic-angular";
+import { SplashPage } from "./splash";
+import { TranslateModule } from "@ngx-translate/core";
 
 @NgModule({
-  declarations: [
-    SplashPage,
-  ],
-  imports: [
-    IonicPageModule.forChild(SplashPage),
-  ],
+  declarations: [SplashPage],
+  imports: [IonicPageModule.forChild(SplashPage), TranslateModule.forChild()]
 })
 export class SplashPageModule {}
