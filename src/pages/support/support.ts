62a27eac32b9df175a67692d82d9a07195ba8fa2
markonnen <jordan.walcaraz@gmail.com>
2018-06-26 11:32:11 -0400

    Added About and EULA - Subpages are generated similarly to Settings page - Created based on passed data and *ngIf in the template

--- src/pages/support/support.ts
+++ src/pages/support/support.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import { TranslateService } from "@ngx-translate/core";
+import { SitePages } from "../index";
 
 /**
  * Generated class for the SupportPage page.
@@ -15,11 +16,32 @@ import { TranslateService } from "@ngx-translate/core";
   templateUrl: "support.html"
 })
 export class SupportPage {
+  aboutSupport = {
+    page: "about",
+    pageTitleKey: "ABOUT_PAGE_TITLE"
+  };
+
+  subSupport: any = SupportPage;
+
+  page: string = "supportMain";
+
+  pageTitleKey: string = "SUPPORT_PAGE_TITLE";
+
+  sitePages: any = SitePages;
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public translate: TranslateService
   ) {}
+
+  ionViewWillEnter() {
+    // Change page attribute to newly pushed page
+    this.page = this.navParams.get("page") || this.page;
+    console.log(this.page);
+    // Change page title to the passed title on navigation
+    this.pageTitleKey = this.navParams.get("pageTitleKey") || this.pageTitleKey;
+  }
 
   ionViewDidLoad() {
     console.log("ionViewDidLoad SupportPage");
