c363c813d81b0b2ee7cc138477cb966050ec4405
Sean Kibbee <seankibbee@gmail.com>
2018-07-04 11:52:03 -0400

    Quick Flag to Enable Prod Mode (#237)
    Forcing a merge as this was done during the period where we had no requirement for signed commits and it's failing with our current requirements

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,6 +2,10 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app.module";
 import { enableProdMode } from "@angular/core";
 
-enableProdMode();
+const pageUrl = window.location.href.toLowerCase();
+
+if (pageUrl.indexOf("localhost") === -1) {
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
