433ba7dd0ffd03f5715b891a8d17ebe47f9f31ea
CalvinRodo <calvinv@gmail.com>
2018-07-06 13:37:34 -0400

    Fixed the tests so they match the new way of creating them

--- src/pages/login-failed/login-failed.spec.ts
+++ src/pages/login-failed/login-failed.spec.ts
@@ -1,32 +1,25 @@
 import { CommonTestModule } from "./../../app/sharedModules";
-import { async, TestBed, ComponentFixture } from "@angular/core/testing";
+import { async, TestBed, inject } from "@angular/core/testing";
 import * as chai from "chai";
 import * as sinon from "sinon";
 import * as sinonChai from "sinon-chai";
 import { LoginFailedPage } from "./login-failed";
 
-let { expect } = chai;
+const { expect } = chai;
 chai.use(sinonChai);
 
 describe("The Login Failed Page", () => {
-  let sut: ComponentFixture<LoginFailedPage>;
-  let comp: LoginFailedPage;
-
-  beforeEach(async () => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: CommonTestModule.getDeclarations([LoginFailedPage]),
       imports: CommonTestModule.getImports(),
-      providers: CommonTestModule.getProviders()
+      providers: CommonTestModule.getProviders([
+        { provide: LoginFailedPage, useClass: LoginFailedPage }
+      ])
     });
-  });
+  }));
 
-  beforeEach(() => {
-    sut = TestBed.createComponent(LoginFailedPage);
-    comp = sut.componentInstance;
-  });
-
-  it("Should be created with no errors", () => {
-    expect(comp).to.exist;
-  });
-
+  it("Should be created with no errors", inject([LoginFailedPage], sut => {
+    expect(sut).to.exist;
+  }));
 });
--- src/providers/provinces/provinces.spec.ts
+++ src/providers/provinces/provinces.spec.ts
@@ -1,25 +1,40 @@
 import { ProvincesProvider } from "../../providers";
+import { async, TestBed, inject } from "@angular/core/testing";
+import { CommonTestModule } from "../../app/sharedModules";
+
 import * as chai from "chai";
 
 const { expect } = chai;
 
 describe("Provinces provider", () => {
-  it("Should return the list of province codes", () => {
-    const provinceProvider: ProvincesProvider = new ProvincesProvider();
-    expect(provinceProvider.provinceCodes).to.have.members([
-      "AB",
-      "BC",
-      "MB",
-      "NB",
-      "NL",
-      "NS",
-      "ON",
-      "PE",
-      "QC",
-      "SK",
-      "NT",
-      "NU",
-      "YT"
-    ]);
-  });
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: CommonTestModule.getDeclarations(),
+      imports: CommonTestModule.getImports(),
+      providers: CommonTestModule.getProviders([
+        { provide: ProvincesProvider, useClass: ProvincesProvider }
+      ])
+    }).compileComponents();
+  }));
+
+  it("Should return the list of province codes", inject(
+    [ProvincesProvider],
+    sut => {
+      expect(sut.provinceCodes).to.have.members([
+        "AB",
+        "BC",
+        "MB",
+        "NB",
+        "NL",
+        "NS",
+        "ON",
+        "PE",
+        "QC",
+        "SK",
+        "NT",
+        "NU",
+        "YT"
+      ]);
+    }
+  ));
 });
