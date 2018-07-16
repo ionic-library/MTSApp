0770e0a2a6661d5b8ab21b77d72813d133657e10
markonnen <jordan.walcaraz@gmail.com>
2018-07-16 09:44:02 -0400

    Removed extra status card component

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from "@angular/core";
 import { HomePageCarouselComponent } from "./home-page-carousel/home-page-carousel";
 import { StatusCardComponent } from "./status-card/status-card";
-import { EiStatusComponent } from "./ei-status/ei-status";
 @NgModule({
-  declarations: [
-    HomePageCarouselComponent,
-    StatusCardComponent,
-    EiStatusComponent
-  ],
+  declarations: [HomePageCarouselComponent, StatusCardComponent],
   imports: [],
-  exports: [HomePageCarouselComponent, StatusCardComponent, EiStatusComponent]
+  exports: [HomePageCarouselComponent, StatusCardComponent]
 })
 export class ComponentsModule {}
