2cce7b7cfa2df08215613ecfb7a3a4c43c525c4a
markonnen <jordan.walcaraz@gmail.com>
2018-07-18 16:10:56 -0400

    Removed RoundProgressComponent import from status-card.ts temporarily

--- src/components/status-card/status-card.ts
+++ src/components/status-card/status-card.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { NavController } from "ionic-angular";
-import { RoundProgressComponent } from "angular-svg-round-progressbar";
+// import { RoundProgressComponent } from "angular-svg-round-progressbar";
 
 /**
  * Generated class for the StatusCardComponent component.
