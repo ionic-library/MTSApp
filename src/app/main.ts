2a6cbd701938c2210368599c20740495d9cb707e
markonnen <jordan.walcaraz@gmail.com>
2018-07-23 11:00:47 -0400

    Uncommented production/devmode check

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,12 +1,12 @@
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app.module";
-// import { enableProdMode } from "@angular/core";
+import { enableProdMode } from "@angular/core";
 
-// const pageUrl = window.location.href.toLowerCase();
+const pageUrl = window.location.href.toLowerCase();
 
-// // ### the 60002 check is temporary, Ionic view app uses localhost address with 60002 port.
-// if (pageUrl.indexOf("localhost") === -1 || pageUrl.indexOf(":60002") > -1) {
-//   enableProdMode();
-// }
+// ### the 60002 check is temporary, Ionic view app uses localhost address with 60002 port.
+if (pageUrl.indexOf("localhost") === -1 || pageUrl.indexOf(":60002") > -1) {
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
