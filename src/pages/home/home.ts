70a402a9b61be1108898aba91ca4679e397620f9
markonnen <jordan.walcaraz@gmail.com>
2018-07-25 10:15:43 -0400

    Removed StatusBar from home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,7 +4,6 @@ import { Component } from "@angular/core";
 import { IonicPage, NavController, ModalController } from "ionic-angular";
 import { SitePages } from "@pages";
 import { TranslateService } from "@ngx-translate/core";
-import { StatusBar } from "../../../node_modules/@ionic-native/status-bar";
 
 /**
  * Home page where user selects services from an
@@ -26,7 +25,6 @@ export class HomePage {
     private readonly navCtrl: NavController,
     private readonly user: User,
     public modalCtrl: ModalController,
-    private readonly statusBar: StatusBar,
     public translate: TranslateService,
     private readonly logProvider: LogProvider
   ) {
@@ -35,7 +33,6 @@ export class HomePage {
     this.getLocation();
     this.getTemperature();
     this.getUserName();
-    this.statusBar.overlaysWebView(true);
   }
 
   private getLocation() {
