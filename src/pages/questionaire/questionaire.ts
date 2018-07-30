324875ce0e1cbebe42ca517075d13046d6b4d09b
markonnen <jordan.walcaraz@gmail.com>
2018-07-30 09:39:11 -0400

    Changed navigation from Questionnaire 1 to customed method - Added first navigation confirmation - Implemented Nav Guard/Confirmation through IonViewCanLeave - Alert will animate out before beginning navigation animation

--- src/pages/questionaire/questionaire.ts
+++ src/pages/questionaire/questionaire.ts
@@ -9,6 +9,7 @@ import {
 import { SitePages } from "@pages";
 import { Logger } from "winston";
 import { LogProvider } from "@providers";
+import { AlertController } from "ionic-angular";
 
 @IonicPage()
 @Component({
@@ -20,11 +21,14 @@ export class QuestionairePage {
 
   pushPage: any;
 
+  allowedToLeave: boolean = false;
+
   constructor(
     public translate: TranslateService,
     public navParams: NavParams,
     public navCtrl: NavController,
     public modalCtrl: ModalController,
+    public alertCtrl: AlertController,
     private readonly logProvider: LogProvider
   ) {
     this.logger = this.logProvider.getLogger();
@@ -33,6 +37,52 @@ export class QuestionairePage {
 
   ionViewDidLoad() {
     this.logger.info("ionViewDidLoad QuestionairePage");
+  }
+
+  ionViewCanLeave() {
+    if (!this.allowedToLeave) {
+      return new Promise<any>((resolve, reject) => {
+        const alert = this.alertCtrl.create({
+          title: "Are you sure you want to leave this page?",
+          message: "Current progress will be lost.",
+          buttons: [
+            {
+              text: "Cancel",
+              role: "cancel",
+              handler: () => {
+                alert
+                  .dismiss()
+                  .then(reject)
+                  .catch((reason: any) => this.logger.error(reason));
+                return false;
+              }
+            },
+            {
+              text: "Continue",
+              handler: () => {
+                alert
+                  .dismiss()
+                  .then(resolve)
+                  .catch((reason: any) => this.logger.error(reason));
+                return false;
+              }
+            }
+          ]
+        });
+        alert.present().catch((reason: any) => this.logger.error(reason));
+      });
+    } else {
+      this.allowedToLeave = false;
+      return true;
+    }
+  }
+
+  nextQuestion() {
+    this.allowedToLeave = true;
+    this.navCtrl
+      .push(this.pushPage)
+      .then(() => this.logger.info("Navigating to : Next question..."))
+      .catch((reason: any) => this.logger.error(reason));
   }
 
   presentHelpModal() {
